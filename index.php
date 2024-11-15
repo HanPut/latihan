d9c44344ac1fd17914ef9d233798dda47ccdd467
HanPut <hanap0802@gmail.com>
2024-11-15 21:20:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+
+    include 'konek.php';
+    ?>
+</head>
+<body>
+    <p> Nama:  <?php echo  $nama ?> </p>
+    <p> NIM:  <?php echo  $nim ?> </p>
+    <p> Kelas:  <?php echo  $kelas ?> </p>
+
+</body>
+</html>
---
+++ konek.php
@@ -0,0 +1,6 @@
+<?php
+$nim = "312310542";
+$nama = "Hana Putra Ramadhan";
+$kelas = "TI.23.CA1";
+
+?>
